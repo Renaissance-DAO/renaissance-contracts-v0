619abd2af6a9cacbd600d9d3f85cfd23a5cdc52d
0xluckyg <scottsgcho@gmail.com>
2022-07-05 00:47:26 +0900

    fix: deploy FNFTCollectionFactory

--- deploy/010_FNFTCollectionFactory.ts
+++ deploy/010_FNFTCollectionFactory.ts
@@ -1,119 +1,43 @@
 import {HardhatRuntimeEnvironment} from 'hardhat/types';
 import {DeployFunction} from 'hardhat-deploy/types';
-import { ethers } from 'ethers';
-import { testnets } from '../utils/constants';
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
-  const {deployments, getNamedAccounts, ethers, getChainId} = hre;
-  const {Contract, getSigner} = ethers;
+  const {deployments, getNamedAccounts, ethers} = hre;
 
   const {deploy, get} = deployments;
-  const chainId = await getChainId();
-  let { WETH, deployer, TREASURY, UNISWAP_V2_FACTORY } = await getNamedAccounts();
-  if (testnets.includes(chainId)) {
-    const mockWETH = await get('WETH');
-    WETH = mockWETH.address;
-  }
+  const {deployer} = await getNamedAccounts();
 
-  const signer = await getSigner(deployer);
+  const signer = await ethers.getSigner(deployer);
+
+  // get ifo factory proxy address
+  const proxyControllerInfo = await get('MultiProxyController');
+  const proxyController = new ethers.Contract(
+    proxyControllerInfo.address,
+    proxyControllerInfo.abi,
+    signer
+  );
+  const vaultManagerAddress = (await proxyController.proxyMap(
+    ethers.utils.formatBytes32String("VaultManager")
+  ))[1];
 
   // deploy implementation contract
-  const vaultImpl = await deploy('FNFTCollection', {
-    from: deployer,
-    log: true,
-  });
-
-  const lpStakingImpl = await deploy('LPStaking', {
-    from: deployer,
-    log: true,
-  });
-
-  const inventoryStakingImpl = await deploy('InventoryStaking', {
-    from: deployer,
-    log: true,
-  });
-
-  const feeDistributorImpl = await deploy('FeeDistributor', {
-    from: deployer,
-    log: true,
-  });
-
-  const factoryImpl = await deploy('FNFTCollectionFactory', {
-    from: deployer,
-    log: true,
-  });
-
-  const stakingTokenProviderImpl = await deploy('StakingTokenProvider', {
+  const fnftCollectionFactoryImpl = await deploy('FNFTCollectionFactory', {
     from: deployer,
     log: true,
   });
 
   // deploy proxy contract
   const deployerInfo = await get('Deployer')
-  const deployerContract = new Contract(
+  const deployerContract = new ethers.Contract(
     deployerInfo.address,
     deployerInfo.abi,
     signer
   );
-
-  const stakingTokenProviderTx = await deployerContract.deployStakingTokenProvider(
-    stakingTokenProviderImpl.address,
-    UNISWAP_V2_FACTORY,
-    WETH,
-    "x" // default prefix
+  await deployerContract.deployFNFTCollectionFactory(
+    fnftCollectionFactoryImpl.address,
+    vaultManagerAddress
   );
-  const stakingTokenProviderReceipt = await stakingTokenProviderTx.wait();
-  let event = stakingTokenProviderReceipt.events.find((event: ethers.Event) => event.event === "ProxyDeployed");
-  const [,stakingTokenProviderAddress,] = event.args;
 
-  const lpStakingTx = await deployerContract.deployLPStaking(
-    lpStakingImpl.address,
-    stakingTokenProviderAddress
-  );
-  const lpStakingReceipt = await lpStakingTx.wait();
-  event = lpStakingReceipt.events.find((event: ethers.Event) => event.event === "ProxyDeployed");
-  const [,lpStakingAddress,] = event.args;
-
-  const feeDistributorTx = await deployerContract.deployFeeDistributor(
-    feeDistributorImpl.address,
-    lpStakingAddress,
-    TREASURY
-  );
-  const feeDistributorReceipt = await feeDistributorTx.wait();
-  event = feeDistributorReceipt.events.find((event: ethers.Event) => event.event === "ProxyDeployed");
-  const [,feeDistributorAddress,] = event.args;
-
-  const fnftCollectionFactoryReceipt = await deployerContract.deployFNFTCollectionFactory(
-    factoryImpl.address,
-    feeDistributorAddress
-  );
-  event = fnftCollectionFactoryReceipt.events.find((event: ethers.Event) => event.event === "ProxyDeployed");
-  const [,fnftCollectionFactoryAddress,] = event.args;
-
-  const inventoryStakingTx = await deployerContract.deployInventoryStaking(
-    inventoryStakingImpl.address,
-    fnftCollectionFactoryAddress
-  );
-  const inventoryStakingReceipt = await inventoryStakingTx.wait();
-  event = inventoryStakingReceipt.events.find((event: ethers.Event) => event.event === "ProxyDeployed");
-  const [,inventoryStakingAddress,] = event.args;
-
-
-  const feeDistributorInfo = await get('FeeDistributor');
-  const feeDistributorContract = new Contract(
-    feeDistributorAddress,
-    feeDistributorInfo.abi,
-    signer
-  );
-  await feeDistributorContract.setFNFTCollectionFactory(factoryImpl.address);
-
-  const lpStakingInfo = await get('LPStaking');
-  const lpStakingContract = new Contract(
-    lpStakingAddress,
-    lpStakingInfo.abi,
-    signer
-  );
-  await lpStakingContract.setFNFTCollectionFactory(factoryImpl.address);
 };
 
 func.tags = ['main', 'local', 'seed'];
